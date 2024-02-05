f2ce6827660e81ef5ec0e10a08f52359eb9650c4
Sh1chiro <pavel.dereyes@gmail.com>
2024-02-05 13:22:28 +0200

    start

---
+++ src/main/java/ua/sh1chiro/TelegramBot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ua.sh1chiro.TelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+
+}
